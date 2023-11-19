d832e41d8d64b97bd2c316d717917ed3869f17ec
gfuselier <gisellefuselier@gmail.com>
2023-11-19 14:46:56 -0600

    wrote putDb and getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,11 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-//don't forget its just one
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database - DONE
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-// id = 1 
+// TODO: Add logic for a method that gets all the content from the database - DONE
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
 initdb();
